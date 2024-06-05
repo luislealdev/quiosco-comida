9e9d1952a7d8f1bce17dce83a277d2e3412885b5
Luis Leal <luisrrleal@gmail.com>
2024-06-05 09:53:20 -0600

    Start working with handleSubmit

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,12 +1,15 @@
 import { AddProductForm } from "@/components/products/AddProductForm"
+import ProductForm from "@/components/products/ProductForm"
 import { Heading } from "@/components/ui/Heading"
 
 const AdminNewProduct = () => {
     return (
-        <section className="flex column" style={{width:'100%', height:'100%'}}>
+        <section className="flex column" style={{ width: '100%', height: '100%' }}>
             <Heading>Nuevo producto</Heading>
             <div className="flex align-center justify-content">
-                <AddProductForm />
+                <AddProductForm>
+                    <ProductForm />
+                </AddProductForm>
             </div>
         </section>
     )
--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import ProductForm from './ProductForm'
+"use client";
 
-export const AddProductForm = () => {
+import { ProductSchema } from "@/src/schema";
+import { toast } from "react-toastify";
+
+export const AddProductForm = ({ children }: { children: React.ReactNode }) => {
+
+    const handleSubmit = async (formData: FormData) => {
+        const data = {
+            name: formData.get('name'),
+            price: formData.get('price'),
+            categoryId: formData.get('categoryId')
+        }
+
+        const result = ProductSchema.safeParse(data);
+        if (!result.success) {
+            result.error.errors.forEach((error) => {
+                toast.error(error.message);
+            });
+            return;
+        }
+    }
+
     return (
         <div className='bg-white mt-10 p-50 radius-30 shadow'>
-            <form action="" className='flex column '>
-                <ProductForm />
-                <input type="submit" className='white-text bg-blue ph-20 p-10 mt-10 bold no-border' style={{cursor:'pointer'}} value='Registrar producto' />
+            <form action={handleSubmit} className='flex column '>
+                {children}
+                <input type="submit" className='white-text bg-blue ph-20 p-10 mt-10 bold no-border' style={{ cursor: 'pointer' }} value='Registrar producto' />
             </form>
         </div>
     )
